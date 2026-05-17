c57114003cc8fc6b4f9a540c33f09e749cd9430f
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-17 13:05:20 -0700

    add initial student API routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 
+const studentRoutes=require("./routes/studentRoutes");
+
+
 const app=express();
 
 app.use(express.json());
+app.use("/students",studentRoutes);
 
 module.export=app;
