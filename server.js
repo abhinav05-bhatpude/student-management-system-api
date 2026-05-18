74cce664dce8ceba3bf70eea6a246f1842dfc1a6
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-18 13:14:38 -0700

    add JSON-based student data persistence

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express=require("express");
 
 const app=express();
 
+app.use(express.json());
+
 const PORT=5000;
 
 const students=[
@@ -17,7 +19,19 @@ app.get("/",(req,res)=>{
 })
 app.get("/students",(req,res)=>{
     res.json(students);
-})
+});
+
+app.post("/students",(req,res)=>{
+    const newStudent=req.body;
+
+    students.push(newStudent);
+
+    res.json({
+        message:"Student added succesfully",
+        student:newStudent,
+    });
+});
+
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 });
