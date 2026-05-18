dd8bdb6e9c0aab8dbde5434c69427fd80e016ba2
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-18 12:07:55 -0700

    add student GET route

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const PORT=5000;
 app.get("/",(req,res)=>{
     res.send("Server Running");
 })
-
+app.get("/students",(req,res)=>{
+    res.send("All Students");
+})
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 });
