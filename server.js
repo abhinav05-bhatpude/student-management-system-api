d4bed9b98e688017a653d4dd4178371181ce707e
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-18 12:09:40 -0700

    send student data as JSON response

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const app=express();
 
 const PORT=5000;
 
+const students=[
+    {
+        id:1,
+        name:"Abhinav",
+        course:"Computer Engineering",
+    },
+];
+
 app.get("/",(req,res)=>{
     res.send("Server Running");
 })
 app.get("/students",(req,res)=>{
-    res.send("All Students");
+    res.json(students);
 })
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
