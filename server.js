ef2e6d9f7a347593ce2bbd395edaf49f1f00a7ba
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-18 12:06:25 -0700

    create basic express server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const express=require("express");
 
-
-const app=require("./src/app");
+const app=express();
 
 const PORT=5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
+app.get("/",(req,res)=>{
+    res.send("Server Running");
 })
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+});
