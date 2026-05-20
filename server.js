9d866399897f2dc3e130b95d505b6363139ade3c
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-20 16:16:28 -0700

    add basic request logger middleware

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ const students = [
     },
 ];
 
+app.use((req,res,next) => {
+    console.log(`${req.method} ${req.url}`);
+
+    next();
+});
 // Home Route
 app.get("/", (req, res) => {
     res.send("Server Running");
